60cd3901685320433e2e42c59efd55f6b903b736
晁兴雨 <chaoxingyu@outlook.com>
2017-12-13 17:54:07 +0800

    上传文件格式化
    上传文件格式化

--- bootdemo/src/main/java/com/cxy/bootdemo/configurations/MultipartConfiguration.java
+++ bootdemo/src/main/java/com/cxy/bootdemo/configurations/MultipartConfiguration.java
@@ -4,12 +4,22 @@ import javax.servlet.MultipartConfigElement;
 
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 
-//@Configuration
+/**
+ * 
+ * @package com.cxy.bootdemo.configurations
+ * @type MultipartConfiguration
+ * @description 上传文件配置
+ * @author cxy
+ * @date 2017年12月13日 下午5:50:37
+ * @version 1.0.0
+ */
+// @Configuration
 public class MultipartConfiguration {
 
 	/**
 	 * springboot 上传文件大小限制解决方法2 
 	 * 解决方法1:在配置文件中设置参数
+	 * 
 	 * @return
 	 */
 	// @Bean
