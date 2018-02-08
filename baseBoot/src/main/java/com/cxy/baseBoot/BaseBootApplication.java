e0f5b2a995f86c4698d7454c5162337fc7da139e
ChaoXingyu <cxy_dream@aliyun.com>
2018-02-08 14:47:47 +0800

    springboot 1.5.10  初始化项目

---
+++ baseBoot/src/main/java/com/cxy/baseBoot/BaseBootApplication.java
@@ -0,0 +1,12 @@
+package com.cxy.baseBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseBootApplication.class, args);
+	}
+}
