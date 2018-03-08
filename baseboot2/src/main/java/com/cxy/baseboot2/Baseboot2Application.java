2926c38bfe55f7b4e86ba6c0c1a50d55efab3d72
ChaoXingyu <cxy_dream@aliyun.com>
2018-03-08 17:03:51 +0800

    springboot 2.0.0

---
+++ baseboot2/src/main/java/com/cxy/baseboot2/Baseboot2Application.java
@@ -0,0 +1,12 @@
+package com.cxy.baseboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Baseboot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Baseboot2Application.class, args);
+	}
+}
