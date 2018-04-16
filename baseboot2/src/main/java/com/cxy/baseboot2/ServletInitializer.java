7d4a1589b2788e5c5d8a54c957c21123dde184cc
ChaoXingyu <cxy_dream@aliyun.com>
2018-04-16 13:42:25 +0800

    修改 pom文件
    改为 war  去掉一些加载项

---
+++ baseboot2/src/main/java/com/cxy/baseboot2/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.cxy.baseboot2;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Baseboot2Application.class);
+	}
+
+}
